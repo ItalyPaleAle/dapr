eb8ba00eb4b631bdd63197e0ba7477c86142615f
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2022-06-01 17:18:01 +0000

    Added mock code
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>

--- pkg/testing/pubsub_mock.go
+++ pkg/testing/pubsub_mock.go
@@ -2,6 +2,7 @@ package testing
 
 import (
 	"context"
+	"sync"
 
 	mock "github.com/stretchr/testify/mock"
 
@@ -11,22 +12,48 @@ import (
 // MockPubSub is a mock pub-sub component object.
 type MockPubSub struct {
 	mock.Mock
+	handlers map[string]pubsub.Handler
+	lock     sync.RWMutex
 }
 
 // Init is a mock initialization method.
 func (m *MockPubSub) Init(metadata pubsub.Metadata) error {
+	m.handlers = make(map[string]pubsub.Handler)
+	m.lock = sync.RWMutex{}
+
 	args := m.Called(metadata)
 	return args.Error(0)
 }
 
 // Publish is a mock publish method.
 func (m *MockPubSub) Publish(req *pubsub.PublishRequest) error {
+	if req.Topic != "" {
+		m.lock.RLock()
+		handler, ok := m.handlers[req.Topic]
+		if ok && handler != nil {
+			msg := &pubsub.NewMessage{
+				Data:        req.Data,
+				Topic:       req.Topic,
+				Metadata:    req.Metadata,
+				ContentType: req.ContentType,
+			}
+			handler(context.Background(), msg)
+		}
+		m.lock.RUnlock()
+	}
+
 	args := m.Called(req)
 	return args.Error(0)
 }
 
 // Subscribe is a mock subscribe method.
 func (m *MockPubSub) Subscribe(req pubsub.SubscribeRequest, handler pubsub.Handler) error {
+	if req.Topic != "" {
+		m.lock.Lock()
+		m.handlers[req.Topic] = handler
+		m.lock.Unlock()
+	}
+
 	args := m.Called(req, handler)
 	return args.Error(0)
 }
