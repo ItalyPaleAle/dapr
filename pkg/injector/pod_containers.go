8b074f395db974144c4fd49667f8a37e681f1e63
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2023-05-18 23:45:17 +0000

    More WIP
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>

--- pkg/injector/pod_containers.go
+++ pkg/injector/pod_containers.go
@@ -16,17 +16,12 @@ package injector
 import (
 	"fmt"
 
-	"golang.org/x/sync/singleflight"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	"github.com/dapr/dapr/pkg/apis/components/v1alpha1"
 	"github.com/dapr/dapr/pkg/injector/annotations"
 	"github.com/dapr/dapr/pkg/injector/components"
 )
-
-// namespaceFlight deduplicates requests to the same namespace
-var namespaceFlight singleflight.Group
 
 func (i *injector) splitContainers(pod corev1.Pod) (appContainers map[int]corev1.Container, componentContainers map[int]corev1.Container, injectedComponentContainers []corev1.Container, err error) {
 	an := annotations.New(pod.Annotations)
@@ -41,13 +36,13 @@ func (i *injector) splitContainers(pod corev1.Pod) (appContainers map[int]corev1
 		// the pod will include a redundant container that should be removed when this code is executed again.
 		// To resolve this issue, it is important to ensure that the same component list is used both here and in runtime,
 		// such as by passing it as an environment variable or populating a volume with an init container.
-		componentsList, err, _ := namespaceFlight.Do(pod.Namespace, func() (any, error) {
-			return i.daprClient.ComponentsV1alpha1().Components(pod.Namespace).List(metav1.ListOptions{})
-		})
+		componentsList, err := i.daprClient.ComponentsV1alpha1().
+			Components(pod.Namespace).
+			List(metav1.ListOptions{})
 		if err != nil {
 			return nil, nil, nil, fmt.Errorf("error when fetching components: %w", err)
 		}
-		injectedComponentContainers = components.Injectable(an.GetString(annotations.KeyAppID), componentsList.(*v1alpha1.ComponentList).Items)
+		injectedComponentContainers = components.Injectable(an.GetString(annotations.KeyAppID), componentsList.Items)
 	}
 	appContainers, componentContainers = components.SplitContainers(pod)
 	return
