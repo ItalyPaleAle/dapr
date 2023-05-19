30fd9b4fd7d263f132c2686cb1246d77b23b6a5a
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2023-05-19 22:00:16 +0000

    Set default values using reflection
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>

--- pkg/injector/patcher/patcher.go
+++ pkg/injector/patcher/patcher.go
@@ -42,33 +42,33 @@ type PodPatcher struct {
 	Enabled                             bool   `annotation:"dapr.io/enabled"`
 	AppPort                             int32  `annotation:"dapr.io/app-port"`
 	Config                              string `annotation:"dapr.io/config"`
-	AppProtocol                         string `annotation:"dapr.io/app-protocol"`
+	AppProtocol                         string `annotation:"dapr.io/app-protocol" default:"http"`
 	AppSSL                              bool   `annotation:"dapr.io/app-ssl"` // TODO: Deprecated in Dapr 1.11; remove in a future Dapr version
 	AppID                               string `annotation:"dapr.io/app-id"`
 	EnableProfiling                     bool   `annotation:"dapr.io/enable-profiling"`
-	LogLevel                            string `annotation:"dapr.io/log-level"`
+	LogLevel                            string `annotation:"dapr.io/log-level" default:"info"`
 	APITokenSecret                      string `annotation:"dapr.io/api-token-secret"`
 	AppTokenSecret                      string `annotation:"dapr.io/app-token-secret"`
 	LogAsJSON                           bool   `annotation:"dapr.io/log-as-json"`
 	AppMaxConcurrency                   int32  `annotation:"dapr.io/app-max-concurrency"`
-	EnableMetrics                       bool   `annotation:"dapr.io/enable-metrics"`
-	MetricsPort                         int32  `annotation:"dapr.io/metrics-port"`
+	EnableMetrics                       bool   `annotation:"dapr.io/enable-metrics" default:"true"`
+	MetricsPort                         int32  `annotation:"dapr.io/metrics-port" default:"9090"`
 	EnableDebug                         bool   `annotation:"dapr.io/enable-debug"`
-	DebugPort                           int32  `annotation:"dapr.io/debug-port"`
+	DebugPort                           int32  `annotation:"dapr.io/debug-port" default:"40000"`
 	Env                                 string `annotation:"dapr.io/env"`
 	SidecarCPURequest                   string `annotation:"dapr.io/sidecar-cpu-request"`
 	SidecarCPULimit                     string `annotation:"dapr.io/sidecar-cpu-limit"`
 	SidecarMemoryRequest                string `annotation:"dapr.io/sidecar-memory-request"`
 	SidecarMemoryLimit                  string `annotation:"dapr.io/sidecar-memory-limit"`
-	SidecarListenAddresses              string `annotation:"dapr.io/sidecar-listen-addresses"`
-	SidecarLivenessProbeDelaySeconds    int32  `annotation:"dapr.io/sidecar-liveness-probe-delay-seconds"`
-	SidecarLivenessProbeTimeoutSeconds  int32  `annotation:"dapr.io/sidecar-liveness-probe-timeout-seconds"`
-	SidecarLivenessProbePeriodSeconds   int32  `annotation:"dapr.io/sidecar-liveness-probe-period-seconds"`
-	SidecarLivenessProbeThreshold       int32  `annotation:"dapr.io/sidecar-liveness-probe-threshold"`
-	SidecarReadinessProbeDelaySeconds   int32  `annotation:"dapr.io/sidecar-readiness-probe-delay-seconds"`
-	SidecarReadinessProbeTimeoutSeconds int32  `annotation:"dapr.io/sidecar-readiness-probe-timeout-seconds"`
-	SidecarReadinessProbePeriodSeconds  int32  `annotation:"dapr.io/sidecar-readiness-probe-period-seconds"`
-	SidecarReadinessProbeThreshold      int32  `annotation:"dapr.io/sidecar-readiness-probe-threshold"`
+	SidecarListenAddresses              string `annotation:"dapr.io/sidecar-listen-addresses" default:"[::1],127.0.0.1"`
+	SidecarLivenessProbeDelaySeconds    int32  `annotation:"dapr.io/sidecar-liveness-probe-delay-seconds" default:"3"`
+	SidecarLivenessProbeTimeoutSeconds  int32  `annotation:"dapr.io/sidecar-liveness-probe-timeout-seconds" default:"3"`
+	SidecarLivenessProbePeriodSeconds   int32  `annotation:"dapr.io/sidecar-liveness-probe-period-seconds" default:"6"`
+	SidecarLivenessProbeThreshold       int32  `annotation:"dapr.io/sidecar-liveness-probe-threshold" default:"3"`
+	SidecarReadinessProbeDelaySeconds   int32  `annotation:"dapr.io/sidecar-readiness-probe-delay-seconds" default:"3"`
+	SidecarReadinessProbeTimeoutSeconds int32  `annotation:"dapr.io/sidecar-readiness-probe-timeout-seconds" default:"3"`
+	SidecarReadinessProbePeriodSeconds  int32  `annotation:"dapr.io/sidecar-readiness-probe-period-seconds" default:"6"`
+	SidecarReadinessProbeThreshold      int32  `annotation:"dapr.io/sidecar-readiness-probe-threshold" default:"3"`
 	SidecarImage                        string `annotation:"dapr.io/sidecar-image"`
 	SidecarSeccompProfileTyle           string `annotation:"dapr.io/sidecar-seccomp-profile-type"`
 	HTTPMaxRequestSize                  int32  `annotation:"dapr.io/http-max-request-size"`
@@ -80,10 +80,10 @@ type PodPatcher struct {
 	VolumeMountsRW                      string `annotation:"dapr.io/volume-mounts-rw"`
 	DisableBuiltinK8sSecretStore        bool   `annotation:"dapr.io/disable-builtin-k8s-secret-store"`
 	EnableAppHealthCheck                bool   `annotation:"dapr.io/enable-app-health-check"`
-	AppHealthCheckPath                  string `annotation:"dapr.io/app-health-check-path"`
-	AppHealthProbeInterval              int32  `annotation:"dapr.io/app-health-probe-interval"` // In seconds
-	AppHealthProbeTimeout               int32  `annotation:"dapr.io/app-health-probe-timeout"`  // In milliseconds
-	AppHealthThreshold                  int32  `annotation:"dapr.io/app-health-threshold"`
+	AppHealthCheckPath                  string `annotation:"dapr.io/app-health-check-path" default:"/healthz"`
+	AppHealthProbeInterval              int32  `annotation:"dapr.io/app-health-probe-interval" default:"5"`  // In seconds
+	AppHealthProbeTimeout               int32  `annotation:"dapr.io/app-health-probe-timeout" default:"500"` // In milliseconds
+	AppHealthThreshold                  int32  `annotation:"dapr.io/app-health-threshold" default:"3"`
 	PlacementHostAddress                string `annotation:"dapr.io/placement-host-address"`
 	PluggableComponents                 string `annotation:"dapr.io/pluggable-components"`
 	PluggableComponentsSocketsFolder    string `annotation:"dapr.io/pluggable-components-sockets-folder"`
@@ -94,31 +94,25 @@ type PodPatcher struct {
 
 // NewPodPatcher returns a PodPatcher object with the default values set.
 func NewPodPatcher() *PodPatcher {
-	return &PodPatcher{
-		LogLevel:                            "info",
-		LogAsJSON:                           false,
-		AppSSL:                              false,
-		AppProtocol:                         "http",
-		EnableMetrics:                       true,
-		MetricsPort:                         9090,
-		EnableDebug:                         false,
-		DebugPort:                           40000,
-		SidecarListenAddresses:              "[::1],127.0.0.1",
-		SidecarLivenessProbeDelaySeconds:    3,
-		SidecarLivenessProbeTimeoutSeconds:  3,
-		SidecarLivenessProbePeriodSeconds:   6,
-		SidecarLivenessProbeThreshold:       3,
-		SidecarReadinessProbeDelaySeconds:   3,
-		SidecarReadinessProbeTimeoutSeconds: 3,
-		SidecarReadinessProbePeriodSeconds:  6,
-		SidecarReadinessProbeThreshold:      3,
-		EnableProfiling:                     false,
-		DisableBuiltinK8sSecretStore:        false,
-		EnableAppHealthCheck:                false,
-		AppHealthCheckPath:                  "/healthz",
-		AppHealthProbeInterval:              5,
-		AppHealthProbeTimeout:               500,
-		AppHealthThreshold:                  3,
+	p := &PodPatcher{}
+	p.setDefaultValues()
+	return p
+}
+
+func (p *PodPatcher) setDefaultValues() {
+	// Iterate through the fields using reflection
+	val := reflect.ValueOf(p).Elem()
+	for i := 0; i < val.NumField(); i++ {
+		fieldT := val.Type().Field(i)
+		fieldV := val.Field(i)
+		an := fieldT.Tag.Get("annotation")
+		def := fieldT.Tag.Get("default")
+		if !fieldV.CanSet() || an == "" || def == "" {
+			continue
+		}
+
+		// Assign the default value
+		setValueFromString(fieldT.Type.Kind(), fieldV, def)
 	}
 }
 
@@ -127,14 +121,10 @@ func (p *PodPatcher) SetFromAnnotations(an map[string]string) {
 	// Iterate through the fields using reflection
 	val := reflect.ValueOf(p).Elem()
 	for i := 0; i < val.NumField(); i++ {
+		fieldV := val.Field(i)
 		fieldT := val.Type().Field(i)
 		key := fieldT.Tag.Get("annotation")
-		if key == "" {
-			continue
-		}
-
-		fieldV := val.Field(i)
-		if !fieldV.CanSet() {
+		if !fieldV.CanSet() || key == "" {
 			continue
 		}
 
@@ -144,45 +134,56 @@ func (p *PodPatcher) SetFromAnnotations(an map[string]string) {
 		}
 
 		// Assign the value
-		switch fieldT.Type.Kind() {
-		case reflect.String:
-			fieldV.SetString(an[key])
-		case reflect.Bool:
-			fieldV.SetBool(utils.IsTruthy(an[key]))
-		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			v, err := strconv.ParseInt(an[key], 10, 64)
-			if err == nil {
-				fieldV.SetInt(v)
-			}
-		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-			v, err := strconv.ParseUint(an[key], 10, 64)
-			if err == nil {
-				fieldV.SetUint(v)
-			}
+		setValueFromString(fieldT.Type.Kind(), fieldV, an[key])
+	}
+}
+
+func setValueFromString(rk reflect.Kind, rv reflect.Value, val string) {
+	switch rk {
+	case reflect.String:
+		rv.SetString(val)
+	case reflect.Bool:
+		rv.SetBool(utils.IsTruthy(val))
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		v, err := strconv.ParseInt(val, 10, 64)
+		if err == nil {
+			rv.SetInt(v)
+		}
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		v, err := strconv.ParseUint(val, 10, 64)
+		if err == nil {
+			rv.SetUint(v)
 		}
 	}
 }
 
 // String implements fmt.Stringer and is used to print the state of the object, primarily for debugging purposes.
 func (p *PodPatcher) String() string {
+	return p.toString(false)
+}
+
+// StringAll returns the list of all annotations (including empty ones), primarily for debugging purposes.
+func (p *PodPatcher) StringAll() string {
+	return p.toString(true)
+}
+
+func (p *PodPatcher) toString(includeAll bool) string {
 	res := strings.Builder{}
 
 	// Iterate through the fields using reflection
 	val := reflect.ValueOf(p).Elem()
 	for i := 0; i < val.NumField(); i++ {
 		fieldT := val.Type().Field(i)
+		fieldV := val.Field(i)
 		key := fieldT.Tag.Get("annotation")
+		def := fieldT.Tag.Get("default")
 		if key == "" {
 			continue
 		}
 
-		fieldV := val.Field(i)
-		if !fieldV.CanSet() {
-			continue
-		}
-
+		// Do not print default values or zero values when there's no default
 		val := cast.ToString(fieldV.Interface())
-		if val != "" {
+		if includeAll || (def != "" && val != def) || (def == "" && !fieldV.IsZero()) {
 			res.WriteString(key + ": " + strconv.Quote(val) + "\n")
 		}
 	}
--- pkg/injector/patcher/patcher_test.go
+++ pkg/injector/patcher/patcher_test.go
@@ -21,17 +21,32 @@ import (
 	"github.com/dapr/dapr/pkg/injector/annotations"
 )
 
+func TestPodPatcherInit(t *testing.T) {
+	p := NewPodPatcher()
+
+	// Ensure default values are set (and that those without a default value are zero)
+	// Check properties of supported kinds: bools, strings, ints
+	assert.Equal(t, "", p.Config)
+	assert.Equal(t, "info", p.LogLevel)
+	assert.Equal(t, int32(0), p.AppPort)
+	assert.Equal(t, int32(9090), p.MetricsPort)
+	assert.False(t, p.EnableProfiling)
+	assert.True(t, p.EnableMetrics)
+}
+
 func TestPodPatcherSetFromAnnotations(t *testing.T) {
 	p := NewPodPatcher()
 
 	// Set properties of supported kinds: bools, strings, ints
 	p.SetFromAnnotations(map[string]string{
-		annotations.KeyEnabled: "1",
-		annotations.KeyAppID:   "myappid",
-		annotations.KeyAppPort: "9876",
+		annotations.KeyEnabled:     "1", // Will be cast using utils.IsTruthy
+		annotations.KeyAppID:       "myappid",
+		annotations.KeyAppPort:     "9876",
+		annotations.KeyMetricsPort: "6789", // Override default value
 	})
 
 	assert.True(t, p.Enabled)
 	assert.Equal(t, "myappid", p.AppID)
 	assert.Equal(t, int32(9876), p.AppPort)
+	assert.Equal(t, int32(6789), p.MetricsPort)
 }
