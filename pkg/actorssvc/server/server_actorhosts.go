322f9d5b2efcf8f9749ad6e5018f2b46f5ed7b6c
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2023-09-19 23:25:24 +0000

    Working on ConnectHost
    Signed-off-by: ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>

--- pkg/actorssvc/server/server_actorhosts.go
+++ pkg/actorssvc/server/server_actorhosts.go
@@ -15,6 +15,10 @@ package server
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"io"
+	"time"
 
 	actorsv1pb "github.com/dapr/dapr/pkg/proto/actors/v1"
 )
@@ -22,8 +26,84 @@ import (
 // ConnectHost is used by the Dapr sidecar to register itself as an actor host.
 // It remains active as a long-lived bi-di stream to allow for the Actors service
 // to communicate with the sidecar.
-func (s *server) ConnectHost(ss actorsv1pb.Actors_ConnectHostServer) error {
-	panic("unimplemented")
+func (s *server) ConnectHost(stream actorsv1pb.Actors_ConnectHostServer) error {
+	// Receive the first message
+	msg, err := s.connectHostReceiveFirstMessage(stream)
+	if err != nil {
+		// If the error is io.EOF, it means that the stream has ended
+		if errors.Is(err, io.EOF) {
+			return nil
+		}
+		log.Warnf("Error receiving first message in ConnectHost: %v", err)
+		return err
+	}
+
+	// Register the actor host
+	actorHostID, err := s.store.AddActorHost(stream.Context(), msg.ToActorStoreRequest())
+	if err != nil {
+		log.Errorf("Failed to register actor host: %v", err)
+		return fmt.Errorf("failed to register actor host: %w", err)
+	}
+
+	log.Debugf("Registered actor host: id='%s' appID='%s' address='%s'", actorHostID, msg.AppId, msg.Address)
+
+	select {
+	case <-stream.Context().Done():
+		log.Debugf("Actor host '%s' has disconnected - unregistering", actorHostID)
+		// Use a background context here because the client has already disconnected
+		err = s.store.RemoveActorHost(context.Background(), actorHostID)
+		if err != nil {
+			log.Errorf("Failed to un-register actor host: %v", err)
+			return fmt.Errorf("failed to un-register actor host: %w", err)
+		}
+	}
+
+	return nil
+}
+
+// Receives the first message sent on connectHost, with a timeout.
+func (s *server) connectHostReceiveFirstMessage(stream actorsv1pb.Actors_ConnectHostServer) (*actorsv1pb.RegisterActorHost, error) {
+	// To start, we expect callers to send a message of type RegisterActorHost
+	// We give callers 5s before disconnecting them
+	msgCh := make(chan *actorsv1pb.RegisterActorHost)
+	defer close(msgCh)
+	errCh := make(chan error)
+	defer close(errCh)
+	go func() {
+		cs, err := stream.Recv()
+		if err != nil {
+			errCh <- err
+			return
+		}
+
+		msg := cs.GetRegisterActorHost()
+		if msg == nil {
+			// If msg is nil, it was not a RegisterActorHost message
+			errCh <- errors.New("received an unexpected first message from caller: expecting a RegisterActorHost")
+			return
+		}
+		msgCh <- msg
+	}()
+
+	ctx, cancel := context.WithTimeout(stream.Context(), 5*time.Second)
+	defer cancel()
+	select {
+	case msg := <-msgCh:
+		// Ensure required fields are present
+		err := msg.ValidateFirstMessage()
+		if err != nil {
+			return nil, err
+		}
+		return msg, nil
+	case <-ctx.Done():
+		// If the error is context canceled, it means the client disconnected, so we convert it to io.EOF (just like when the stream ends)
+		if errors.Is(ctx.Err(), context.Canceled) {
+			return nil, io.EOF
+		}
+		return nil, errors.New("timed out while waiting for the first message")
+	case err := <-errCh:
+		return nil, fmt.Errorf("error while receiving first message: %w", err)
+	}
 }
 
 // LookupActor returns the address of an actor.
---
+++ pkg/proto/actors/v1/actors_additional.go
@@ -0,0 +1,72 @@
+/*
+Copyright 2023 The Dapr Authors
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package actors
+
+import (
+	"errors"
+
+	"github.com/dapr/components-contrib/actorstore"
+)
+
+// This file contains additional, hand-written methods added to the generated objects.
+
+// ValidateFirstMessage validates if the message contains all fields that are required in the first message.
+func (x *RegisterActorHost) ValidateFirstMessage() error {
+	const errPrefix = "first message validation failed: "
+	if x == nil {
+		return errors.New(errPrefix + "message is nil")
+	}
+	if x.Address == "" {
+		return errors.New(errPrefix + "required property 'address' is not set")
+	}
+	if x.AppId == "" {
+		return errors.New(errPrefix + "required property 'app_id' is not set")
+	}
+	if x.ApiLevel <= 0 {
+		return errors.New(errPrefix + "required property 'api_level' is not set")
+	}
+	return nil
+}
+
+// ToActorStoreRequest converts the message to an actorstore.AddActorHostRequest object.
+func (x *RegisterActorHost) ToActorStoreRequest() actorstore.AddActorHostRequest {
+	var actorTypes []actorstore.ActorHostType = nil
+	if x.ActorTypes != nil {
+		var n int
+		actorTypes = make([]actorstore.ActorHostType, len(x.ActorTypes))
+		for _, at := range x.ActorTypes {
+			if at == nil {
+				continue
+			}
+			actorTypes[n] = at.ToActorStoreRequest()
+			n++
+		}
+		actorTypes = actorTypes[:n]
+	}
+
+	return actorstore.AddActorHostRequest{
+		AppID:      x.AppId,
+		Address:    x.Address,
+		ApiLevel:   x.ApiLevel,
+		ActorTypes: actorTypes,
+	}
+}
+
+// ToActorStoreRequest converts the message to an actorstore.ActorHostType object.
+func (x *ActorHostType) ToActorStoreRequest() actorstore.ActorHostType {
+	return actorstore.ActorHostType{
+		ActorType:   x.ActorType,
+		IdleTimeout: x.IdleTimeout,
+	}
+}
